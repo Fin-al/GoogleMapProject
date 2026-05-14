b8430a23eef98be6c7c3c5d52c18f978ead0039f
Fin-al <finaltbm@gmail.com>
2026-05-14 16:06:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var locations = [
     bounds: { north: 34.24080, south: 34.24010, east: -118.52600, west: -118.52720 }
   },
 
-/*  Locations */
+/*  Locations  */
   {
     name:   "Bayramian Hall",
     bounds: { north: 34.24075, south: 34.23995, east: -118.53075, west: -118.53190 }
